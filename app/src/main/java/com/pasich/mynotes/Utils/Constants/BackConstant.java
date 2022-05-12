321fe788a7a8aa5c34d555c003844687b2809092
pasichDev <apasichnik9@gmail.com>
2022-05-12 18:48:39 +0200

    #10 (FragmentMain)
    Signed-off-by: pasichDev <apasichnik9@gmail.com>

--- app/src/main/java/com/pasich/mynotes/Utils/Constants/BackConstant.java
+++ app/src/main/java/com/pasich/mynotes/Utils/Constants/BackConstant.java
@@ -1,5 +1,6 @@
 package com.pasich.mynotes.Utils.Constants;
 
+@Deprecated
 public class BackConstant {
   public static boolean UPDATE_THEME = false;
   public static boolean UPDATE_LISTVIEW = false;
