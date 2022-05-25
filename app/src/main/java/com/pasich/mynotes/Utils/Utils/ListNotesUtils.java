d94f042c52cbc39186e33ec46d9d37bb5939e9d2
pasichDev <apasichnik9@gmail.com>
2022-05-25 10:21:22 +0200

    #37

--- app/src/main/java/com/pasich/mynotes/Utils/Utils/ListNotesUtils.java
+++ app/src/main/java/com/pasich/mynotes/Utils/Utils/ListNotesUtils.java
@@ -13,7 +13,7 @@ public class ListNotesUtils {
    * @return - date (string)
    */
   public static String returnDateFile(Date date) {
-    return new SimpleDateFormat("dd.MM.yyyy  HH:mm", Locale.getDefault()).format(date);
+    return new SimpleDateFormat("dd.MM.yyyy  HH:mm:ss", Locale.getDefault()).format(date);
   }
 
 }
