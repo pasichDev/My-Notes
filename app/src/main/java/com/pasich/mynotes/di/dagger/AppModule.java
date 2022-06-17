fffeb3a9c9fcf735d6f766518a5da6459457cef4
pasichDev <apasichnik9@gmail.com>
2022-06-17 17:59:22 +0200

    #87

--- app/src/main/java/com/pasich/mynotes/di/dagger/AppModule.java
+++ app/src/main/java/com/pasich/mynotes/di/dagger/AppModule.java
@@ -2,6 +2,8 @@ package com.pasich.mynotes.di.dagger;
 
 import android.content.Context;
 
+import com.pasich.mynotes.data.DataManager;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -20,4 +22,9 @@ public class AppModule {
     return context;
   }
 
+  @AppScope
+  @Provides
+  DataManager providerDataManager() {
+    return new DataManager();
+  }
 }
