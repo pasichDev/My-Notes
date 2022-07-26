8c55a51a576b141a48a013b2e119dcc47e5e449d
pasichDev <apasichnik9@gmail.com>
2022-07-26 22:09:45 +0300

    #104
    Signed-off-by: pasichDev <apasichnik9@gmail.com>

--- app/src/main/java/com/pasich/mynotes/ui/view/dialogs/main/TagDialog/TagDialogView.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/dialogs/main/TagDialog/TagDialogView.java
@@ -1,38 +1,66 @@
 package com.pasich.mynotes.ui.view.dialogs.main.TagDialog;
 
 import android.view.LayoutInflater;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.pasich.mynotes.R;
 import com.pasich.mynotes.ui.view.customView.InputTagView;
 import com.pasich.mynotes.utils.recycler.SpacesItemDecoration;
 
 public class TagDialogView extends InputTagView {
 
-  public final RecyclerView listTags;
+    public final RecyclerView listTags;
 
-  public TagDialogView(LayoutInflater inflater) {
-    super(inflater);
-    this.listTags = new RecyclerView(getContextRoot());
-    initialization();
+    public TagDialogView(LayoutInflater inflater) {
+        super(inflater);
+        this.listTags = new RecyclerView(getContextRoot());
+        initialization();
 
-  }
+    }
 
-  private void initialization() {
-    addTitle("");
-    initializeListTags();
-    addView(getNewTagView());
-  }
+    private void initialization() {
+        addTitle("");
+        LinearLayout linearLayoutRecycle = new LinearLayout(getContextRoot());
+        linearLayoutRecycle.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayoutRecycle.addView(createRemoveTagButton());
+        linearLayoutRecycle.addView(createRecycleView());
+        addView(linearLayoutRecycle, getLp());
+        addView(getNewTagView());
+    }
 
-  private void initializeListTags(){
-    listTags.addItemDecoration(new SpacesItemDecoration(5));
-    listTags.setLayoutManager(
-            new LinearLayoutManager(getContextRoot(), RecyclerView.HORIZONTAL, false));
-    LinearLayout.LayoutParams LP = new LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-    LP.setMargins(30, 0, 10, 0);
-    addView(listTags, LP);
-  }
+    private RecyclerView createRecycleView() {
+        listTags.addItemDecoration(new SpacesItemDecoration(5));
+        listTags.setLayoutManager(
+                new LinearLayoutManager(getContextRoot(), RecyclerView.HORIZONTAL, false));
+        return listTags;
+    }
+
+
+    private ImageButton createRemoveTagButton() {
+        ImageButton imageCLeanTag = new ImageButton(getContextRoot());
+        imageCLeanTag.setImageResource(R.drawable.ic_close_search_view);
+        imageCLeanTag.setId(R.id.removeTagForDialog);
+        imageCLeanTag.setScaleType(ImageView.ScaleType.CENTER);
+        imageCLeanTag.setBackground(null);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 10, 0, 0);
+
+        imageCLeanTag.setLayoutParams(layoutParams);
+
+        return imageCLeanTag;
+    }
+
+    private LinearLayout.LayoutParams getLp() {
+        LinearLayout.LayoutParams LP = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LP.setMargins(30, 0, 10, 0);
+        return LP;
+    }
 }
