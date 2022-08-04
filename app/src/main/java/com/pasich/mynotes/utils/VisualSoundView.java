8368300962fa3fbcb1dc6428e12d05b21746365b
pasichDev <apasichnik9@gmail.com>
2022-08-04 22:52:07 +0300

    #131
    Signed-off-by: pasichDev <apasichnik9@gmail.com>

---
+++ app/src/main/java/com/pasich/mynotes/utils/VisualSoundView.java
@@ -0,0 +1,23 @@
+package com.pasich.mynotes.utils;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+class VisualSoundView extends View {
+    public VisualSoundView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+
+}
