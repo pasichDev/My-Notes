a74d3c4ba4d9a994680f2e8577ddf5876d443877
pasichDev <apasichnik9@gmail.com>
2022-08-07 17:02:09 +0300

    #110
    Signed-off-by: pasichDev <apasichnik9@gmail.com>

--- app/src/main/java/com/pasich/mynotes/ui/view/dialogs/note/MoreNoteDialog.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/dialogs/note/MoreNoteDialog.java
@@ -1,6 +1,7 @@
 package com.pasich.mynotes.ui.view.dialogs.note;
 
 import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
@@ -16,6 +17,7 @@ import com.pasich.mynotes.data.model.ChoiceModel;
 import com.pasich.mynotes.data.notes.Note;
 import com.pasich.mynotes.utils.GoogleTranslationIntent;
 import com.pasich.mynotes.utils.ShareUtils;
+import com.pasich.mynotes.utils.ShortCutUtils;
 import com.pasich.mynotes.utils.adapters.DialogListAdapter;
 
 import java.util.ArrayList;
@@ -43,19 +45,8 @@ public class MoreNoteDialog extends DialogFragment {
                         AnimationUtils.loadAnimation(listView.getContext(), R.anim.item_animation_dialog)));
         listView.setDivider(null);
 
-        arraySortOption.add(
-                new ChoiceModel(getString(R.string.share), R.drawable.ic_share, "Share", false));
-        arraySortOption.add(
-                new ChoiceModel(
-                        getString(R.string.translateNote), R.drawable.ic_translate, "GoogleTanslationIntent", false));
+        initList(arraySortOption);
 
-        if (!typeActivity) {
-            arraySortOption.add(
-                    new ChoiceModel(getString(R.string.trashNotes), R.drawable.ic_delete, "Delete", false));
-        }
-        arraySortOption.add(
-                new ChoiceModel(
-                        getString(R.string.noSave), R.drawable.ic_close_search_view, "Close", false));
         DialogListAdapter adapter = new DialogListAdapter(arraySortOption);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(
@@ -64,8 +55,8 @@ public class MoreNoteDialog extends DialogFragment {
                         assert noteActivityView != null;
                         noteActivityView.closeActivityNotSaved();
                     }
-                    if (adapter.getItem(position).getAction().equals("GoogleTanslationIntent")) {
-                        new GoogleTranslationIntent().startTranslation(getActivity(), mNote.getValue());
+                    if (adapter.getItem(position).getAction().equals("GoogleTranslationIntent")) {
+                        new GoogleTranslationIntent().startTranslation(requireActivity(), mNote.getValue());
                     }
                     if (adapter.getItem(position).getAction().equals("Share")) {
                         new ShareUtils(mNote, getActivity()).shareNotes();
@@ -74,9 +65,33 @@ public class MoreNoteDialog extends DialogFragment {
                         assert noteActivityView != null;
                         noteActivityView.deleteNote(mNote);
                     }
+                    if (adapter.getItem(position).getAction().equals("addShotCut") &&
+                            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        ShortCutUtils.createShortCut(mNote, getContext());
+                    }
                     dismiss();
                 });
         builder.setContentView(listView);
         return builder;
+    }
+
+    private void initList(ArrayList<ChoiceModel> arraySortOption) {
+        arraySortOption.add(
+                new ChoiceModel(getString(R.string.share), R.drawable.ic_share, "Share", false));
+
+        if (mNote.getValue().length() >= 5) arraySortOption.add(
+                new ChoiceModel(
+                        getString(R.string.translateNote), R.drawable.ic_translate, "GoogleTranslationIntent", false));
+
+        if (!typeActivity) {
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                arraySortOption.add(new ChoiceModel(getString(R.string.addShortCutLauncher), R.drawable.ic_label, "addShotCut", false));
+            }
+            arraySortOption.add(
+                    new ChoiceModel(getString(R.string.trashNotes), R.drawable.ic_delete, "Delete", false));
+        }
+        arraySortOption.add(
+                new ChoiceModel(
+                        getString(R.string.noSave), R.drawable.ic_close_search_view, "Close", false));
     }
 }
