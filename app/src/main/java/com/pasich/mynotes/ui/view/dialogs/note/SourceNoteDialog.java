ee59305e75f46e465ad175c8e8f7ce63fd7f0aae
pasichDev <apasichnik9@gmail.com>
2022-08-11 20:13:18 +0300

    #136
    Signed-off-by: pasichDev <apasichnik9@gmail.com>

--- app/src/main/java/com/pasich/mynotes/ui/view/dialogs/note/SourceNoteDialog.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/dialogs/note/SourceNoteDialog.java
@@ -1,7 +1,13 @@
 package com.pasich.mynotes.ui.view.dialogs.note;
 
 import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -60,24 +66,33 @@ public class SourceNoteDialog extends DialogFragment {
 
         listDialogView.getItemsView().setOnItemClickListener(
                 (parent, v, position, id) -> {
-                    //   if(sourceAdapter.getItem(position).getType().equals("Mail")){
-
-
-
-                         /*   Intent intent = new Intent(Intent.ACTION_SENDTO);
-                            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-                            intent.putExtra(Intent.EXTRA_EMAIL, "addresses");
-                            intent.putExtra(Intent.EXTRA_SUBJECT, sourceAdapter.getItem(position).getSource());
-                       //     if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
-                                startActivity(intent);
-                        //    }
-*/
-                    //   }
-                    //  dismiss();
-
-
+                    Intent intent = null;
+                    if (sourceAdapter.getItem(position).getType().equals("Mail")) {
+                        intent = new Intent(Intent.ACTION_SENDTO)
+                                .setData(Uri.parse("mailto:" + sourceAdapter.getItem(position).getSource()));
+                    }
+                    if (sourceAdapter.getItem(position).getType().equals("Url")) {
+                        intent = new Intent(Intent.ACTION_VIEW, Uri.parse(sourceAdapter.getItem(position).getSource()));
+                    }
+                    if (sourceAdapter.getItem(position).getType().equals("Tel")) {
+                        intent = new Intent(Intent.ACTION_DIAL)
+                                .setData(Uri.parse("tel:" + sourceAdapter.getItem(position).getSource()));
+                    }
+                    assert intent != null;
+                    if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
                 });
 
+        listDialogView.getItemsView().setOnItemLongClickListener((parent, v, position, id) -> {
+            String selectedItem = sourceAdapter.getItem(position).getSource();
+            ClipboardManager clipboard =
+                    (ClipboardManager) requireActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard.setPrimaryClip(ClipData.newPlainText(selectedItem, selectedItem));
+            Toast.makeText(requireContext(), getString(R.string.copyX) + " " + selectedItem, Toast.LENGTH_SHORT)
+                    .show();
+            return true;
+        });
 
         listDialogView.getRootContainer().addView(listDialogView.getItemsView());
         builder.setContentView(listDialogView.getRootContainer());
@@ -87,7 +102,6 @@ public class SourceNoteDialog extends DialogFragment {
     private void chooseItem(int position) {
         filterSourceAdapter.chooseItem(position);
         sourceAdapter.refreshList(searchSourceNote.getListSources(filterSourceAdapter.getItem(position).getType()));
-
     }
 
 
@@ -109,4 +123,5 @@ public class SourceNoteDialog extends DialogFragment {
         filterList.add(new SourceFilterModel(getString(R.string.mail), "Mail"));
         return filterList;
     }
+
 }
