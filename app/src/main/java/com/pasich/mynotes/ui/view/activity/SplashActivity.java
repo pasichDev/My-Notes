6bb7d3effe9ae1454675a055619ee63290a6223e
PasichDev <67899666+pasichDev@users.noreply.github.com>
2022-10-08 21:41:21 +0300

    #223
    Signed-off-by: PasichDev <67899666+pasichDev@users.noreply.github.com>

--- app/src/main/java/com/pasich/mynotes/ui/view/activity/SplashActivity.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/activity/SplashActivity.java
@@ -8,7 +8,6 @@ import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.pasich.mynotes.BuildConfig;
 import com.pasich.mynotes.R;
 import com.pasich.mynotes.utils.base.simplifications.AnimationListener;
 
@@ -24,24 +23,24 @@ public class SplashActivity extends AppCompatActivity {
 
   private void startAnimation() {
 
-      if (BuildConfig.DEBUG) {
-          startNextActivity();
-      } else {
-          Animation mAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.spalsh_start_activity_animation);
-          mAnimation.setAnimationListener(new AnimationListener() {
-              @Override
-              public void animationEnd() {
-                  startNextActivity();
-              }
-          });
-          findViewById(R.id.splashImage).startAnimation(mAnimation);
-      }
+      ///   if (BuildConfig.DEBUG) {
+      //       startNextActivity();
+      //   } else {
+      Animation mAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.spalsh_start_activity_animation);
+      mAnimation.setAnimationListener(new AnimationListener() {
+          @Override
+          public void animationEnd() {
+              startNextActivity();
+          }
+      });
+      findViewById(R.id.splashImage).startAnimation(mAnimation);
   }
+    // }
 
 
   private void startNextActivity() {
-    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-    finish();
+      startActivity(new Intent(SplashActivity.this, HelloActivity.class));
+      finish();
   }
 
 }
