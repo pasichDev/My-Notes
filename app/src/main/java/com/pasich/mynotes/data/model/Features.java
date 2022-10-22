976cf2ab9b8da8a379163df637db6a9118ba8163
PasichDev <67899666+pasichDev@users.noreply.github.com>
2022-10-22 21:59:54 +0300

    #165
    Signed-off-by: PasichDev <67899666+pasichDev@users.noreply.github.com>

---
+++ app/src/main/java/com/pasich/mynotes/data/model/Features.java
@@ -0,0 +1,45 @@
+package com.pasich.mynotes.data.model;
+
+public class Features {
+
+    private int Title;
+    private int Image;
+    private int Info;
+    private boolean button;
+
+    public Features create(int Title, int Image, int Info, boolean button) {
+        this.Title = Title;
+        this.Image = Image;
+        this.Info = Info;
+        this.button = button;
+        return this;
+    }
+
+    public int getImage() {
+        return Image;
+    }
+
+    public int getTitle() {
+        return Title;
+    }
+
+    public int getInfo() {
+        return Info;
+    }
+
+    public void setImage(int image) {
+        Image = image;
+    }
+
+    public void setTitle(int title) {
+        Title = title;
+    }
+
+    public void setInfo(int info) {
+        Info = info;
+    }
+
+    public boolean isButton() {
+        return button;
+    }
+}
--- app/src/main/java/com/pasich/mynotes/ui/view/helloUI/fragments/FeaturesFragment.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/helloUI/fragments/FeaturesFragment.java
@@ -6,21 +6,38 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.pasich.mynotes.R;
+import com.pasich.mynotes.data.model.Features;
 import com.pasich.mynotes.ui.view.helloUI.tool.HelloTool;
+import com.pasich.mynotes.utils.adapters.FeaturesPageAdapter;
+
+import java.util.ArrayList;
 
 
 public class FeaturesFragment extends Fragment {
 
     private HelloTool helloTool;
-
+    private ViewPager2 listFeatures;
+    private FeaturesPageAdapter featuresPageAdapter;
+    private ArrayList<Features> listFeaturesArray = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         helloTool = (HelloTool) getContext();
 
+        listFeaturesArray.add(new Features().create(R.string.feature_desing_title,
+                R.drawable.features_desing, R.string.feature_desing_info, false));
+        listFeaturesArray.add(new Features().create(R.string.feature_tags_title,
+                R.drawable.features_tags, R.string.feature_tags_info, false));
+
+        listFeaturesArray.add(new Features().create(R.string.feature_create_title,
+                R.drawable.feature_creates, R.string.feature_create_info, false));
+
+        listFeaturesArray.add(new Features().create(R.string.feature_newFetures_title,
+                R.drawable.feature_new, R.string.feature_newFetures_info, true));
     }
 
     @Override
@@ -29,7 +46,15 @@ public class FeaturesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_features, container, false);
         view.findViewById(R.id.backFragment).setOnClickListener(v -> helloTool.backFragment(2));
         view.findViewById(R.id.backupFragmentNext).setOnClickListener(v -> helloTool.nextFragment(2));
+        listFeatures = view.findViewById(R.id.featuresViewPage);
+        initFeaturesList();
         return view;
+
+    }
+
+    private void initFeaturesList() {
+        featuresPageAdapter = new FeaturesPageAdapter(requireActivity(), listFeaturesArray);
+        listFeatures.setAdapter(featuresPageAdapter);
 
     }
 }
