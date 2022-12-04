7c413b1b58e11e0d2cba3aca19423a9a5dc4505e
PasichDev <67899666+pasichDev@users.noreply.github.com>
2022-12-04 21:59:20 +0200

    #305
    Signed-off-by: PasichDev <67899666+pasichDev@users.noreply.github.com>

--- app/src/main/java/com/pasich/mynotes/ui/view/activity/ShareActivity.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/activity/ShareActivity.java
@@ -1,6 +1,7 @@
 package com.pasich.mynotes.ui.view.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -8,30 +9,75 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.pasich.mynotes.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * An activity that is a gateway to save a note via the save button
  */
 public class ShareActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = getIntent();
 
-    if (getIntent().getType().equals("text/plain")) {
-      startActivity(new Intent(this, NoteActivity.class).putExtra("NewNote", true).putExtra("tagNote", "").putExtra("shareText", handleSendText()));
-      finish();
-    } else {
-      Toast.makeText(this, getString(R.string.notSupportedShare), Toast.LENGTH_LONG).show();
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            try {
+                startNoteActivityIntent(readFile(getIntent().getData()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (intent.getType().equals("text/plain")) {
+
+            startNoteActivityIntent(handleSendText());
+
+        } else {
+            Toast.makeText(this, getString(R.string.notSupportedShare), Toast.LENGTH_LONG).show();
+        }
+
+        finish();
     }
-  }
 
-  /**
-   * Returns the received text from the heap
-   *
-   * @return - String (TextData share)
-   */
-  private String handleSendText() {
-    String sharedText = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-    return sharedText != null ? sharedText : "";
-  }
+
+    /**
+     * Method that implements the opening of an intent to create an annotation
+     *
+     * @param textShare - The text that we fumble in a note
+     */
+    private void startNoteActivityIntent(String textShare) {
+        startActivity(new Intent(this, NoteActivity.class).putExtra("NewNote", true).putExtra("tagNote", "").putExtra("shareText", textShare));
+    }
+
+
+    /**
+     * The method that opens the file from which we want to take the text for sharing
+     *
+     * @param uri - uri file
+     * @return - text files share
+     * @throws IOException - errors
+     */
+    private String readFile(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getContentResolver().openInputStream(uri)));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append('\n');
+        }
+
+        return stringBuilder.toString();
+    }
+
+
+    /**
+     * Returns the received text from the heap
+     *
+     * @return - String (TextData share)
+     */
+    private String handleSendText() {
+        String sharedText = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        return sharedText != null ? sharedText : "";
+    }
 }
