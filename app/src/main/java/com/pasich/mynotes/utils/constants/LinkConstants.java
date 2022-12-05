53c4c5ffa7c352af11f0e501fc4b3ca46eb69eb9
PasichDev <67899666+pasichDev@users.noreply.github.com>
2022-12-06 00:06:36 +0200

    fix help
    Signed-off-by: PasichDev <67899666+pasichDev@users.noreply.github.com>

--- app/src/main/java/com/pasich/mynotes/utils/constants/LinkConstants.java
+++ app/src/main/java/com/pasich/mynotes/utils/constants/LinkConstants.java
@@ -1,7 +1,7 @@
 package com.pasich.mynotes.utils.constants;
 
 public class LinkConstants {
-    public static final String LINK_HOW_TO_USE = "https://pasichdev.github.io/pasich/privacyMyNotes.html";
+    public static final String LINK_HOW_TO_USE = "https://pasichdev.github.io/pasich/help.html";
     public static final String LINK_PRIVACY_POLICY = "https://pasichdev.github.io/pasich/privacyMyNotes.html";
     public static final String LINK_TELEGRAM_DEVELOP = "https://t.me/andriipasichnik";
 }
