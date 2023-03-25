a5d04cd1675605bed76892efef7f91bc402f1ff9
PasichDev <67899666+pasichDev@users.noreply.github.com>
2023-03-25 22:11:15 +0200

    #412
    Signed-off-by: PasichDev <67899666+pasichDev@users.noreply.github.com>

--- app/src/main/java/com/pasich/mynotes/utils/constants/ContactLink.java
+++ app/src/main/java/com/pasich/mynotes/utils/constants/ContactLink.java
@@ -4,5 +4,5 @@ public class ContactLink {
     public static final String LINK_PRIVACY_POLICY = "https://mynotes.pasichdev.pw/privacypolicy/";
     public static final String LINK_TELEGRAM_DEVELOP = "https://t.me/pasichDev";
     public static final String LINK_APP_SITE = "https://mynotes.pasichdev.pw/";
-    public static final String LINK_MONOBANK_DONATE = "https://send.monobank.ua/jar/8meT5NGSyA";
+    public static final String LINK_MONOBANK_DONATE = "https://send.monobank.ua/jar/9dE8qvUFrr";
 }
